83f079863a9a97fdb28b31b4dc35a6b0b3de197a
Diego Benatez Elaas <dbenitez@zeta33.aulas.gsyc.es>
2015-03-04 17:11:26 +0100

    añadido aleat.py modificado: eliminado su main y modificada su href

---
+++ aleat.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import webapp
+import random
+
+
+class Aleat(webapp.app):
+
+    def process(self, parsedRequest):
+        """Process the relevant elements of the request.
+        Returns the HTTP code for the reply, and an HTML page.
+        """
+        direccion_aleat = random.randint(0, 10000000)
+        html = ("<html><body><h1>Hello World!" +
+                "</h1></body></html>" +
+                '<a href=' "/aleat/"+ str(direccion_aleat) +
+                '>Dame otra </a>' +
+                "\r\n")
+        return ("200 OK", html)
+
+#if __name__ == "__main__":
+   #ulrAleat = Aleat("localhost", 1234)
